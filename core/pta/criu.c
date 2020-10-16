6dd9ffd4ba87281d21193f6c611773710e7e585f
mwerensteijn <mitchellwerensteijn@msn.com>
2020-10-16 08:39:13 +0200

    Now using real register data, program counter and stack pointer from a json checkpoint

--- core/pta/criu.c
+++ core/pta/criu.c
@@ -40,6 +40,25 @@ struct criu_checkpoint {
 	uint64_t tpidr_el0_addr;
 };
 
+enum checkpoint_file_types { 
+	CORE_FILE = 0,				// core-*.img
+	MM_FILE,				// mm-*.img
+	PAGEMAP_FILE,			// pagemap-*.img
+	PAGES_BINARY_FILE,		// pages-*.img
+	EXECUTABLE_BINARY_FILE	// The binary itself that is checkpointed
+};
+
+// Subtract the last enum from the first to determine the number of 
+// elements in the enum. By doing this we can use the enum values as indexes
+// to the checkpoint_files array. Example checkpoint_files[CORE_FILE].
+static const int CHECKPOINT_FILES = EXECUTABLE_BINARY_FILE - CORE_FILE + 1; 
+
+struct checkpoint_file {
+	enum checkpoint_file_types file_type;
+	uint64_t buffer_index;
+	uint64_t file_size;
+};
+
 // This is test data, consisting of instructions that only executes a sys_exit syscall
 // const uint8_t binary_data[4096] __aligned(4096) = { 
 	// 0x00, 0x00, 0x80, 0xd2, 0xa8, 0x0b, 
@@ -53,7 +72,7 @@ static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
   return -1;
 }
 
-static bool parse_checkpoint_core(struct criu_checkpoint * checkpoint, char * json) {
+static bool parse_checkpoint_core(struct criu_checkpoint * checkpoint, char * json, uint64_t file_size) {
 	if(checkpoint == NULL) {
 		DMSG("Error: criu_checkpoint struct is NULL");
 		return false;
@@ -64,13 +83,13 @@ static bool parse_checkpoint_core(struct criu_checkpoint * checkpoint, char * js
 	jsmn_init(&parser);
 
 	// First only determine the number of tokens.
-	int items = jsmn_parse(&parser, json, strlen(json), NULL, 128);
+	int items = jsmn_parse(&parser, json, file_size, NULL, 128);\
 
 	jsmntok_t tokens[items];
 	
 	// Reset position in stream
 	jsmn_init(&parser);
-	int left = jsmn_parse(&parser, json, strlen(json), tokens, items);
+	int left = jsmn_parse(&parser, json, file_size, tokens, items);
 
 	// Invalid file.
 	if (items < 1 || tokens[0].type != JSMN_OBJECT) {
@@ -80,40 +99,32 @@ static bool parse_checkpoint_core(struct criu_checkpoint * checkpoint, char * js
 
 	// Parse the JSON version of the core checkpoint file (example core-2956.img)
 	for(int i = 1; i < items; i++) {
-		if (jsoneq(json, &tokens[i], "regs") == 0) {
-			// Parse all the registers from the checkpoint
+		// Parse all registers
+		if (jsoneq(json, &tokens[i], "regs") == 0) { 
 			if(tokens[i+1].type == JSMN_ARRAY) {
 				for(int y = 0; y < tokens[i+1].size && (y < sizeof(checkpoint->regs)); y++) {
-					unsigned long long value = strtoul(json + tokens[i+y+2].start, NULL, 16);
-					checkpoint->regs[y] = value;
+					checkpoint->regs[y] = strtoul(json + tokens[i+y+2].start, NULL, 16);
 				}
 			}
-		} else if(jsoneq(json, &tokens[i], "vregs") == 0) {
-			// Parse all the VFP registers
+		// Parse all the VFP registers
+		} else if(jsoneq(json, &tokens[i], "vregs") == 0) { 
 			if(tokens[i+1].type == JSMN_ARRAY) {
 				for(int y = 0; y < tokens[i+1].size && (y < sizeof(checkpoint->vregs)); y++) {
-					unsigned long long value = strtoul(json + tokens[i+y+2].start, NULL, 10);
-					checkpoint->vregs[y] = value;
+					checkpoint->vregs[y] = strtoul(json + tokens[i+y+2].start, NULL, 10);
 				}
 			}
-		} else if(jsoneq(json, &tokens[i], "pc") == 0) {
-			// Parse the checkpoint program counter
-			if(tokens[i+1].type == JSMN_STRING) {
-				unsigned long long value = strtoul(json + tokens[i+1].start, NULL, 16);
-				checkpoint->entry_addr = value;
-			}			
-		} else if(jsoneq(json, &tokens[i], "sp") == 0) {
-			// Parse the checkpoint stack pointer
-			if(tokens[i+1].type == JSMN_STRING) {
-				unsigned long long value = strtoul(json + tokens[i+1].start, NULL, 16);
-				checkpoint->stack_addr = value;
-			}			
-		} else if(jsoneq(json, &tokens[i], "tls") == 0) {
-			// Parse the TPIDR_EL0 address
-			if(tokens[i+1].type == JSMN_PRIMITIVE) {
-				unsigned long long value = strtoul(json + tokens[i+1].start, NULL, 10);
-				checkpoint->tpidr_el0_addr = value;
-			}			
+		// Parse the checkpoint program counter
+		} else if(jsoneq(json, &tokens[i], "pc") == 0) { 
+			if(tokens[i+1].type == JSMN_STRING)
+				checkpoint->entry_addr = strtoul(json + tokens[i+1].start, NULL, 16);
+		// Parse the checkpoint stack pointer
+		} else if(jsoneq(json, &tokens[i], "sp") == 0) { 
+			if(tokens[i+1].type == JSMN_STRING)
+				checkpoint->stack_addr = strtoul(json + tokens[i+1].start, NULL, 16);	
+		// Parse the TPIDR_EL0 address
+		} else if(jsoneq(json, &tokens[i], "tls") == 0) { 
+			if(tokens[i+1].type == JSMN_PRIMITIVE)
+				checkpoint->tpidr_el0_addr = strtoul(json + tokens[i+1].start, NULL, 10);
 		}
 	}
 
@@ -213,7 +224,7 @@ void copy_vm_area_data(struct criu_vm_area * area) {
 	memcpy((void *)area->vm_start, area->original_data + area->offset, area->vm_end - area->vm_start);
 }
 
-static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param * pageData) {
+static TEE_Result load_checkpoint_data(TEE_Param * binaryData, TEE_Param * binaryDataInformation) {
 	TEE_Result res;
 	TEE_UUID uuid = CHECKPOINT_UUID;
 
@@ -227,8 +238,9 @@ static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param
 	s->lock_thread = THREAD_ID_INVALID;
 	s->ref_count = 1;
 
+	struct checkpoint_file * checkpoint_file_var = binaryDataInformation->memref.buffer;
 	struct criu_checkpoint checkpoint = {};
-	if(!parse_checkpoint_core(&checkpoint, test_string)) {
+	if(!parse_checkpoint_core(&checkpoint, binaryData->memref.buffer + checkpoint_file_var[CORE_FILE].buffer_index, checkpoint_file_var[CORE_FILE].file_size)) {
 		DMSG("Checkpoint file core-*.img file is not valid.");
 		return TEE_ERROR_BAD_FORMAT;
 	}
@@ -247,7 +259,7 @@ static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param
 	struct criu_vm_area code = {
 		.vm_start		= 0x40050000,
 		.vm_end			= 0x400dd000,
-		.original_data	= checkpointedBinary->memref.buffer,
+		.original_data	= binaryData->memref.buffer + checkpoint_file_var[EXECUTABLE_BINARY_FILE].buffer_index,
 		.offset 		= 0,
 		.protection		= TEE_MATTR_PRW
 	};
@@ -255,7 +267,7 @@ static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param
 	struct criu_vm_area stack = {
 		.vm_start		= 0x7ffca45000,
 		.vm_end			= 0x7ffca48000,
-		.original_data	= pageData->memref.buffer,
+		.original_data	= binaryData->memref.buffer + checkpoint_file_var[PAGES_BINARY_FILE].buffer_index,
 		.offset 		= (40-3)*4096,
 		.protection		= TEE_MATTR_URW | TEE_MATTR_PRW
 	};
@@ -263,7 +275,7 @@ static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param
 	struct criu_vm_area data = {
 		.vm_start		= 0x400de000,
 		.vm_end			= 0x400e2000,
-		.original_data	= checkpointedBinary->memref.buffer,
+		.original_data	= binaryData->memref.buffer + checkpoint_file_var[EXECUTABLE_BINARY_FILE].buffer_index,
 		.offset 		= 577536,
 		.protection		= TEE_MATTR_URW | TEE_MATTR_PRW
 	};
@@ -271,7 +283,7 @@ static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param
 	struct criu_vm_area data0 = {
 		.vm_start		= 0x400e2000,
 		.vm_end			= 0x400e4000,
-		.original_data	= pageData->memref.buffer,
+		.original_data	= binaryData->memref.buffer + checkpoint_file_var[PAGES_BINARY_FILE].buffer_index,
 		.offset 		= (1 * 4096),
 		.protection		= TEE_MATTR_URW | TEE_MATTR_PRW
 	};
@@ -279,7 +291,7 @@ static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param
 	struct criu_vm_area data1 = {
 		.vm_start		= 0x744d247000,
 		.vm_end			= 0x744d248000,
-		.original_data	= pageData->memref.buffer,
+		.original_data	= binaryData->memref.buffer + checkpoint_file_var[PAGES_BINARY_FILE].buffer_index,
 		.offset 		= 4096*34,
 		.protection		= TEE_MATTR_URW | TEE_MATTR_PRW
 	};
@@ -287,7 +299,7 @@ static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param
 	struct criu_vm_area data2 = {
 		.vm_start		= 0x400e5000,
 		.vm_end			= 0x400e9000,
-		.original_data	= pageData->memref.buffer,
+		.original_data	= binaryData->memref.buffer + checkpoint_file_var[PAGES_BINARY_FILE].buffer_index,
 		.offset 		= 4096*3,
 		.protection		= TEE_MATTR_URW | TEE_MATTR_PRW
 	};
@@ -295,7 +307,7 @@ static TEE_Result load_checkpoint_data(TEE_Param * checkpointedBinary, TEE_Param
 	struct criu_vm_area data3 = {
 		.vm_start		= 0x744ce08000,
 		.vm_end			= 0x744ce08000 + (4096 * 2),
-		.original_data	= pageData->memref.buffer,
+		.original_data	= binaryData->memref.buffer + checkpoint_file_var[PAGES_BINARY_FILE].buffer_index,
 		.offset 		= (19 * 4096),
 		.protection		= TEE_MATTR_URW | TEE_MATTR_PRW
 	};
@@ -408,8 +420,13 @@ static TEE_Result criu_load_checkpoint(uint32_t param_types,
 
 	IMSG("Got data from NW, size: %d and %d", params[0].memref.size, params[1].memref.size);
 
-	DMSG("Second argument contains: %s", (char *) params[0].memref.buffer);
-
+	uint8_t files = params[1].memref.size / sizeof(struct checkpoint_file);
+	DMSG("Second argument contains information about %d checkpoint files", files);
+	if(files == CHECKPOINT_FILES)
+		DMSG("Which matches to the expected number: %d/%d", files, CHECKPOINT_FILES);
+	else
+		DMSG("Unexpected number of checkpoint files: %d/%d", files, CHECKPOINT_FILES);
+	
 	// LOAD CHECKPOINT DATA
 	load_checkpoint_data(&params[0], &params[1]);
 	char message[] = "hello from mitchell";
