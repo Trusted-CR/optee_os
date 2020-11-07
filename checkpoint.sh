9a93a1600e8f34040b384d7f888ec8e69b20eb24
mwerensteijn <mitchellwerensteijn@msn.com>
2020-11-07 08:56:34 +0100

    Needs to be tested

--- checkpoint.sh
+++ checkpoint.sh
@@ -8,4 +8,6 @@ then
 else
 	echo "Going to checkpoint $1 with PID $pid"
 fi
-criu dump -t `pidof $1` --shell-job
+criu dump -t `pidof $1` -D checkpoint --shell-job
+./optee_app_migrator $pid $1
+criu restore -D checkpoint --shell-job
