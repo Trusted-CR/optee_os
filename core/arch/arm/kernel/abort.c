4f334364004dcd9f6bcdc09a22ffaa210464ec53
Mitchell Werensteijn <mitchellwerensteijn@msn.com>
2020-12-28 19:01:19 +0100

    This finally solves the absolute last vfp buf I had on the hikey board..

--- core/arch/arm/kernel/abort.c
+++ core/arch/arm/kernel/abort.c
@@ -372,7 +372,7 @@ static void handle_user_ta_panic(struct abort_info *ai)
 	ai->regs->spsr = SPSR_64(SPSR_64_MODE_EL1, SPSR_64_MODE_SP_EL0, daif);
 }
 #endif /*ARM64*/
-
+bool user_vfp_enabled = false;
 #ifdef CFG_WITH_VFP
 static void handle_user_ta_vfp(void)
 {
@@ -386,7 +386,7 @@ static void handle_user_ta_vfp(void)
 	if(uctx->is_criu_checkpoint) {
 		uctx->checkpoint->regs.fp_used = true;
 	}
-
+	user_vfp_enabled = true;
 	thread_user_enable_vfp(&uctx->vfp);
 }
 #endif /*CFG_WITH_VFP*/
@@ -558,27 +558,45 @@ void checkpoint_back(struct thread_abort_regs *regs, uint32_t pc) {
 			// Only backup the floating point registers if the program actually used it
 			// Otherwise wrong values will be backed up. 
 			if(checkpoint->regs.fp_used) {
-				// Temporarily enable vfp to retrieve registers
-				bool vfp_enabled = true;
-				if(!vfp_is_enabled()) {
-					// To restore the original vfp state after reading the registers.
-					vfp_enabled = false;
+				if(ctx->uctx.vfp.saved) {
+					// Checkpoint back the FPCR register
+					checkpoint->regs.fpcr = ctx->uctx.vfp.vfp.fpcr;
+					// Checkpoint back the FPSR register
+					checkpoint->regs.fpsr = ctx->uctx.vfp.vfp.fpsr;
 					
-					// Temporarily enable to retrieve registers.
-					vfp_enable();
+					// Store vfp registers
+					volatile uint64_t * p = NULL;
+					for(uint8_t i = 0, vregs_idx = 0; i < 32; i++) {
+						p = (volatile uint64_t *) &ctx->uctx.vfp.vfp.reg[i].v[0];
+						checkpoint->regs.vregs[vregs_idx++] = *p;
+						p++;
+						checkpoint->regs.vregs[vregs_idx++] = *p;
+					}
+				} else {
+					// Temporarily enable vfp to retrieve registers
+					bool vfp_enabled = true;
+					if(!vfp_is_enabled()) {
+						// The state was probably lazy saved, that means that the values are still in the vfp registers.
+
+						// To restore the original vfp state after reading the registers.
+						vfp_enabled = false;
+						
+						// Temporarily enable to retrieve registers.
+						vfp_enable();
+					}
+
+					// Store vfp registers
+					vfp_save_extension_regs(checkpoint->regs.vregs);
+
+					// Checkpoint back the FPCR register
+					checkpoint->regs.fpcr = read_fpcr();
+					// Checkpoint back the FPSR register
+					checkpoint->regs.fpsr = read_fpsr();				
+
+					// vfp was disabled beforehand, so disable it again.
+					if(!vfp_enabled)
+						vfp_disable();
 				}
-
-				// Store vfp registers
-				vfp_save_extension_regs(checkpoint->regs.vregs);
-
-				// Checkpoint back the FPCR register
-				checkpoint->regs.fpcr = read_fpcr();
-				// Checkpoint back the FPSR register
-				checkpoint->regs.fpsr = read_fpsr();
-
-				// vfp was disabled beforehand, so disable it again.
-				if(!vfp_enabled)
-					vfp_disable();
 			}
 		}
 	}
@@ -618,17 +636,14 @@ void abort_handler(uint32_t abort_type, struct thread_abort_regs *regs)
 			panic("abort outside thread context");
 		}
 
-		if(!in_page_fault) {
-			in_page_fault = true;
-
-			thread_kernel_save_vfp();
-			handled = tee_pager_handle_fault(&ai);
-			thread_kernel_restore_vfp();
-
-			in_page_fault = false;
-		} else {
-			handled = tee_pager_handle_fault(&ai);
+		if(user_vfp_enabled) {
+			thread_user_save_vfp();
+			user_vfp_enabled = false;
 		}
+
+		thread_kernel_save_vfp();
+		handled = tee_pager_handle_fault(&ai);
+		thread_kernel_restore_vfp();
 
 		if (!handled) {
 			struct tee_ta_ctx *ctx = thread_get_tsd()->ctx;
